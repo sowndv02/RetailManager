e196ef80949157d620e3df1aa1e8a05f4b889aab
sowndv02 <daoson03112002@gmail.com>
2023-08-23 08:44:49 +0700

    Connect Model MVVM

---
+++ RMDesktopUserInterface/Boostrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using RMDesktopUserInterface.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUserInterface
+{
+    public class Boostrapper:BootstrapperBase
+    {
+        public Boostrapper() 
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
