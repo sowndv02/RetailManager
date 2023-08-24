b487a825c22df80f972847bf80720b8738db511a
sowndv02 <daoson03112002@gmail.com>
2023-08-24 19:44:35 +0700

    Add WPF project with MVVM Adds our WPF user interface project and configures it with Caliburn Micro for our MVVM framework. Include Simple Containter DI system

--- RMDesktopUserInterface/Boostrapper.cs
+++ RMDesktopUserInterface/Boostrapper.cs
@@ -9,11 +9,33 @@ using System.Windows;
 
 namespace RMDesktopUserInterface
 {
+    // Thiết lập bắt đầu ứng dụng với Caliburn.Micro framework
     public class Boostrapper:BootstrapperBase
     {
+
+        // Tạo container
+        private SimpleContainer _container = new SimpleContainer();
+
+        //Thiết lập môi trường ứng dụng
         public Boostrapper() 
         {
             Initialize();
+        }
+
+        // Tạo các DI 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container.Singleton<IWindowManager, WindowManager>().Singleton<IEventAggregator, EventAggregator>();
+
+            // Kiểm tra các lớp đang chạy
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
